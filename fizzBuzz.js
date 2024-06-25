e9f8aff0a4aed9818aeac0fd4d7a8f0c6409e2be
Reshma Mariam Libu <reshmamariamlibu@gmail.com>
2024-06-25 14:42:22 +0530

    Created a program to for fizzbuzz .first prints a number from 1 to 40 and checks whether the number is divisible by 3,5 and 15, if it is divisible by 3 then print fizz, then if it is divisible by 5 print buzz and finally if the number is divisible by 15 then print fizzbuzz.

---
+++ fizzBuzz.js
@@ -0,0 +1,16 @@
+var num = 40
+for(let i=1;i<=num;i++)
+{
+    if(i%15==0){
+        console.log("fizzbuzz");
+    }
+    else if(i%5==0){
+        console.log("buzz");
+    }
+    else if(i%3==0){
+        console.log("fizz");
+    }
+    else {
+        console.log(i);
+    }
+}
